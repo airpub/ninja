95c579e3934f16e8b0134ad278d978810ba12184
Guo Yu <i@guoyu.me>
2014-08-28 16:18:30 +0800

    0.1: improve workflow

--- bin/build.sh
+++ bin/build.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
 node_modules/.bin/uglifyjs \
-  dist/codemirror.min.js \
-  src/ninja.js \
+  bower_components/codemirror/lib/codemirror.js \
+  bower_components/codemirror/addon/mode/overlay.js \
+  bower_components/codemirror/mode/xml/xml.js \
+  bower_components/codemirror/mode/markdown/markdown.js \
+  bower_components/codemirror/mode/gfm/gfm.js \
+  bower_components/codemirror/mode/javascript/javascript.js \
+  bower_components/codemirror/mode/css/css.js \
+  bower_components/codemirror/mode/htmlmixed/htmlmixed.js \
+  src/javascript/commands.js \
+  src/javascript/ninja.js \
   --mangle \
   --compress \
   -o dist/ninja.min.js \
